2ac1e7c055389a829a9d648c4aa1557820ac1422
Richard L <rail.rich@gmail.com>
2021-12-12 02:22:44 -0500

    D12

--- Solver/Days/Day12.cs
+++ Solver/Days/Day12.cs
@@ -9,11 +9,69 @@ public class Day12 : DayBase
 {
 	public override long Part1(string input)
 	{
-		return 0;
+		var nodes = NewMethod(input);
+		var paths = 0;
+		foreach (var path in nodes["start"])
+			paths += GetPathsToEnd(nodes, path, new string[0], false);
+
+		return paths;
 	}
 
 	public override long Part2(string input)
 	{
-		return 0;
+		var nodes = NewMethod(input);
+		var paths = 0;
+		foreach (var path in nodes["start"])
+			paths += GetPathsToEnd(nodes, path, new string[0], true);
+
+		return paths;
+	}
+
+	private int GetPathsToEnd(Dictionary<string, List<string>> nodes, string currentNode, string[] smallVisited, bool canVisiteOneTwice)
+	{
+		if (currentNode == "start")
+			return 0;
+		if (currentNode == "end")
+			return 1;
+
+		var isSmallCaverne = char.IsLower(currentNode[0]);
+		if (isSmallCaverne && smallVisited.Contains(currentNode))
+			if (!canVisiteOneTwice)
+				return 0;
+			else canVisiteOneTwice = false;
+
+		int paths = 0;
+		var a = smallVisited.ToList();
+		a.Add(currentNode);
+		var newSmallVisited = a.ToArray();
+		foreach (var path in nodes[currentNode])
+			paths += GetPathsToEnd(nodes, path, newSmallVisited, canVisiteOneTwice);
+
+		return paths;
+	}
+
+	private static Dictionary<string, List<string>> NewMethod(string input)
+	{
+		Dictionary<string, List<string>> nodes = new Dictionary<string, List<string>>();
+
+		var paths = input.Split("\n");
+		foreach (var path in paths)
+		{
+			var splited = path.Split("-");
+			var from = splited[0];
+			var to = splited[1];
+
+			if (!nodes.ContainsKey(from))
+				nodes.Add(from, new List<string>());
+			var parent = nodes[from];
+
+			if (!nodes.ContainsKey(to))
+				nodes.Add(to, new List<string>());
+			var child = nodes[to];
+
+			child.Add(from);
+			parent.Add(to);
+		}
+		return nodes;
 	}
 }
